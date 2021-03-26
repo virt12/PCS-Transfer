c7fccc5429ecac723d6a86053ff8c16bcb66f22f
virt12 <34688963+virt12@users.noreply.github.com>
2021-03-26 23:48:05 +0200

    Add files via upload

--- homework_task_pcs_transfer_zymantas.py
+++ homework_task_pcs_transfer_zymantas.py
@@ -1,17 +1,103 @@
-import psycopg2
-import re
+#import psycopg2
+#import re
+#import json
+#import datetime
+
+
+
+
 import json
-import datetime
+import pprint
+import re
 
-conn = psycopg2.connect("dbname=logs user=postgres password=admin")
 
-cur=conn.cursor()
-cur.execute("CREATE TABLE ex_logs4(Some_description VARCHAR, Execution_environment_description VARCHAR, Date_when_benchmark_has_been_performed date, Server_name_where_benchmark_has_been_performed  VARCHAR, Application_version varchar, Test_suite_version varchar, Method_API varchar, Method_name varchar, Parameters varchar,  Testcase_execution_time_in_seconds float  );")
+with open('C:/Users/Žymantas/Desktop/homework/example_log.txt', 'rt') as f:
+    sending_pattern = r"Sending (.*) for reference time measurement"
+    rpc_calls = []
+    for line in f:
+        a = re.search(sending_pattern, line)
+        if a:
+            json_text = a.group(1)
+            rpc_calls.append(json.loads(json_text))
+        else:
+            a = re.search(r'Got response in (.*)s', line)
+            if a:
+                time = a.group(1)
+                rpc_calls[-1]['time'] = float(time)
 
+    pprint.pprint(rpc_calls)
+    pprint.pprint(type(rpc_calls))
+    for a in rpc_calls:
+        #cur.execute("INSERT INTO ex_logs4 (Some_description, Execution_environment_description,Date_when_benchmark_has_been_performed, Server_name_where_benchmark_has_been_performed, Application_version, Test_suite_version,Method_API, Method_name, Parameters,Testcase_execution_time_in_seconds) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (descr1, env1, date1, server1, app1, test1, api1name, met1name, params1, time1))
+
+
+
+
+
+
+
+
+
+
+
+
+#conn = psycopg2.connect("dbname=logs user=postgres password=admin")
+
+#cur=conn.cursor()
+#cur.execute("CREATE TABLE ex_logs4(Some_description VARCHAR, Execution_environment_description VARCHAR, Date_when_benchmark_has_been_performed date, Server_name_where_benchmark_has_been_performed  VARCHAR, Application_version varchar, Test_suite_version varchar, Method_API varchar, Method_name varchar, Parameters varchar,  Testcase_execution_time_in_seconds float  );")
+
+#with open('C:/Users/Žymantas/Desktop/homework/example_log.txt', 'rt') as f:
+    #text = f.read()
+    #pattern = "Sending (.*) for reference time measurement\s+Got response in (.*)s"
+   # a=re.findall(pattern, text)
+   # print(a)
+   # print(type(a))
+   # print(a[0])
+    #print(a[1])
+    #for i in a:
+    #    a[i]=json.loads(a[i])
+    #b=[json.loads(thing) for thing in a]
+    #print(a[1][0])
+    #print(b)
+    #b=json.dumps(a)
+
+    #print(b)
+    #b=json.loads(b)
+    #print(b)
+    #print(type(b))
+    #for i in a:
+    #    print(i[0])
+    #print(type(a))
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 mylines = []                            
 with open ('C:/Users/Žymantas/Desktop/homework/example_log.txt', 'rt') as myfile: 
     for myline in myfile:              
-        mylines.append(myline)           
+        mylines.append(myline)  
+
+
+with open(blah blah blah) as f:
+    text = f.read()
+
+
+'''
+'''         
 
 m8=mylines[8]
 m8=re.sub('Sending for reference time measurement', '', m8)
@@ -84,3 +170,4 @@ conn.commit()
 cur.close()
 
 conn.close()
+'''
